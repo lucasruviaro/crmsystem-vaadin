653e02c3e405d64a58739083f68094ff15c41dbd
lucasruviaro <lucasruviaro@hotmail.com>
2023-01-18 15:12:53 -0300

    projeto finalizado

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -2,6 +2,7 @@ package com.example.application;
 
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @Theme(value = "flowcrmtutorial")
+@PWA(name = "CRM System", shortName = "CRM", offlinePath="offline.html", offlineResources = {"./images/icon.jpg", "./images/offline.jpg"})
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 public class Application implements AppShellConfigurator {
 
